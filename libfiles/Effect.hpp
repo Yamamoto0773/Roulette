2b3250cc58d2f20ceca5afb5aaa6710fe56c4c5a
Nanami Yamamoto <nanamin0634@gmail.com>
2017-12-20 18:27:04 +0900

    add
    エフェクト管理クラス，図形描画クラスを追加

---
+++ libfiles/Effect.hpp
@@ -0,0 +1,67 @@
+﻿#pragma once
+
+#include "CDDPro90.h"
+#include <time.h>
+
+#define TEXTURECUNT (4)		// テクスチャの数
+#define MAXEFFECTCNT (200)	// 一度に描画できるエフェクトの数
+
+typedef struct _EFFECTPOS {
+	float x, y;
+}EFFECTPOS;
+
+typedef struct _EFFECTINFO {
+	unsigned int texID;
+	EFFECTPOS pos;
+	SIZE size;
+	float rotate;
+}EFFECTINFO;
+
+
+class Effect {
+private:
+	EFFECTINFO info;
+
+	EFFECTPOS mvSpeed;
+	float rotateSpeed;
+	
+
+public:
+	Effect(unsigned int texID, EFFECTPOS &pos, SIZE &size, EFFECTPOS &mvSpd, float rotateSpd);
+	~Effect();
+
+	void Move();
+	
+	const EFFECTPOS* GetPosition() const;
+	const EFFECTINFO* GetInfo() const;
+};
+
+
+class EffectManager {
+private:
+	CDDPro90 *dd;
+	Effect *effect[MAXEFFECTCNT];
+
+	unsigned int width, height;
+
+	EffectManager(const EffectManager&) = delete;	// コピー不可
+	const EffectManager& operator=(const EffectManager&) = delete; // 代入不可
+
+public:
+	EffectManager(CDDPro90 *dd, unsigned int screenW, unsigned int screenH);
+	~EffectManager();
+
+	// 更新
+	void Update();
+
+	// 描画
+	void Draw() const;
+
+	int effectCnt;
+
+private:
+	void AddEffect();
+	bool IsFrameout(unsigned int element);
+	void DeleteEffect(unsigned int element);
+
+};
