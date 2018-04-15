ef91e859f79726ea18eb699fadac0313bddc59b0
nanamin <nanamin0634@gmail.com>
2018-04-15 22:20:15 +0900

    add
    CTiimerクラスを追加

---
+++ libfiles/CTimer.cpp
@@ -0,0 +1,51 @@
+﻿#include "CTimer.h"
+
+CTimer::CTimer()
+{
+	if( QueryPerformanceFrequency(&mFreq) ) {
+		// 高解像度カウンターに対応
+		bPerf	= TRUE;
+	} else {
+		// 高解像度カウンターに非対応
+		mFreq.QuadPart	= 1000LL;		// timeGetTime()で代用
+		bPerf	= FALSE;
+	}
+	iFps = 60;
+}
+
+CTimer::~CTimer()
+{
+
+}
+
+BOOL CTimer::Start( int fps )
+{
+	iFps	= fps;
+	iCount	= 0;
+
+	if( bPerf ) {
+		QueryPerformanceCounter( &mStart );
+	} else {
+		mStart.QuadPart = (LONGLONG)timeGetTime();
+	}
+	return TRUE;
+}
+
+int CTimer::Run( void )
+{
+	LARGE_INTEGER now;
+	if( bPerf ) {
+		QueryPerformanceCounter( &now );
+	} else {
+		now.QuadPart = (LONGLONG)timeGetTime();
+	}
+
+	int count = (int)((now.QuadPart - mStart.QuadPart) / (mFreq.QuadPart/iFps) );
+	if( count!=iCount ) {
+		int ret = count - iCount;
+		iCount = count;
+		return ret;
+	}
+
+	return 0;
+}
---
+++ libfiles/CTimer.h
@@ -0,0 +1,27 @@
+﻿#ifndef _CTIMER_H
+#define _CTIMER_H
+///////////////////////////////////////////////////////////////////////////////////
+// CTimer : タイマー処理クラス v2.00                                             //
+//                                                                               //
+// このソースコードは自由に改変して使用可能です。                                //
+// また商用利用も可能ですが、すべての環境で正しく動作する保障はありません。      //
+//                          http://www.charatsoft.com/                           //
+///////////////////////////////////////////////////////////////////////////////////
+#include <Windows.h>
+
+
+class CTimer {
+protected:
+	BOOL			bPerf;			// 高解像度タイマーがサポートされているか
+	LARGE_INTEGER	mFreq;			// システム周波数
+	LARGE_INTEGER	mStart;			// 開始時間
+	int				iFps;			// 割り込みタイミング
+	int				iCount;			// 割り込みカウント数
+public:
+	CTimer();
+	virtual ~CTimer();
+	BOOL Start( int fps=60 );		// 指定のFPSで割り込みスタート
+	int Run( void );				// 割り込みがあれば1以上を返す（割り込み回数）
+};
+
+#endif
